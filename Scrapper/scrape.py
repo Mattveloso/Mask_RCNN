9894e46120fdb370b3e431f41e78f9d35d68fd18
MattVeloso <45905939+Mattveloso@users.noreply.github.com>
2020-05-22 19:15:34 -0300

    Added more functions to data scrapper

--- Scrapper/scrape.py
+++ Scrapper/scrape.py
@@ -26,18 +26,49 @@ def download(url, pathname):
             f.write(data)
             # update the progress bar manually
             progress.update(len(data))
-URLS=[
-'https://www.decathlon.com.br/bicicleta-de-estrada-triban-100-btwin/p',
-'https://www.decathlon.com.br/bicicleta-triban-rc100/p',
-'https://www.decathlon.com.br/bicicleta-infantil-de-estrada-aro-650-triban-100/p',
-'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc120/p',
-'https://www.decathlon.com.br/bicicleta-feminina-de-estrada-triban/p',
-'https://www.decathlon.com.br/bicicleta-estrada-ultra-af900/p',
-'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc500/p',
-'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc520/p',
-'https://www.decathlon.com.br/bicicleta-de-estrada-triban-540-cinza-e-preto/p'
-]
+#Roadbikes
+# URLS=[
+# 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-100-btwin/p',
+# 'https://www.decathlon.com.br/bicicleta-triban-rc100/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-de-estrada-aro-650-triban-100/p',
+# 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc120/p',
+# 'https://www.decathlon.com.br/bicicleta-feminina-de-estrada-triban/p',
+# 'https://www.decathlon.com.br/bicicleta-estrada-ultra-af900/p',
+# 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc500/p',
+# 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-rc520/p',
+# 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-540-cinza-e-preto/p'
+# ]
+
+
 #URL = 'https://www.decathlon.com.br/bicicleta-de-estrada-triban-100-btwin/p'
+
+#Kids_bikes
+URLS =[
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-20--b-twin-original-500-14826/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-de-equilibrio-aro-12-btwin-runride-100-verde/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-14-robot-btwin/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-equilibrio-run-ride-btwin/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-16-btwin-astronaute/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-14-unicornio-btwin/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-de-equilibrio-aro-12-btwin-runride-500-laranja/p',
+# 'https://www.decathlon.com.br/-bicicleta-infantil-aro-20--rockrider-st500-/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-24--rockrider-st500/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-20-rockrider-st120-feminina/p',
+# 'https://www.decathlon.com.br/--btwin-rr-500-branca/p',
+# 'https://www.decathlon.com.br/bicicleta-infantil-aro-14-btwin-mini-monsters/p'
+'https://www.decathlon.com.br/--btwin-rr-500-branca/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-14-btwin-mini-monsters/p',
+'https://www.decathlon.com.br/bicicleta-infantil-de-equilibrio-aro-12-btwin-runride-500-rosa/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-20-racing-boy-btwin/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-16-doctor-girl-btwin/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-16-jack-pirabike-btwin/p',
+'https://www.decathlon.com.br/rockrider-st900-vermelho-24-br0000286/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-20--rockrider-st120-masculina/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-24--poply-500/p',
+'https://www.decathlon.com.br/monster-truck/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-16-btwin-exotic-princess/p',
+'https://www.decathlon.com.br/bicicleta-infantil-aro-20-mistigirl-btwin/p'
+]
 for URL in URLS:
     page=requests.get(URL)
 
@@ -53,4 +84,12 @@ for URL in URLS:
     for pic in pics:
         link = pic['zoom']
         #Bikes.append(link)
-        download(link,'RoadBike')
+        download(link,'Kids_bikes')
+
+# %% Renaming files
+i=58
+for file in os.listdir('Kids_bikes/'):
+    src = 'Kids_bikes/'+file
+    dst = 'Kids_bikes/'+str(i)+'.jpg'
+    i+=1
+    os.rename(src,dst)
